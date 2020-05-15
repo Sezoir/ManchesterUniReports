ac04b55a9d2372782d9c26f8de305ee580e20662
Sezoir <joshmottley0@gmail.com>
2020-05-15 19:06:52 +0100

    - Added empty boxplot class. - Added Text class with function to add "\n" by textwrap.

---
+++ Toolbox/BoxPlot.py
@@ -0,0 +1,22 @@
+## Math libraries
+# Data frames
+import pandas as pd
+# General array math library
+import numpy as np
+# Graph library
+import matplotlib.pyplot as plt
+
+
+class BoxPlot:
+    def __init__(self):
+        return
+
+    def uniqueBoxplot(self, label, values):
+        # Get the unique values
+        unique = pd.unique(label)
+        # Get
+
+        # Create new figure
+        fig = plt.figure()
+        #
+        return
---
+++ Toolbox/Text.py
@@ -0,0 +1,30 @@
+## Math libraries
+# Data frames
+import pandas as pd
+# General array math library
+import numpy as np
+
+import textwrap
+
+
+class Text:
+    def __init__(self):
+        return
+
+    # Add a "\n" after the maxspace limit for the inputted string or list of strings.
+    # @todo add "inplace" option.
+    def addSeperator(self, data, **options):
+        if isinstance(data, str):
+            return self.__Seperator__(data, **options)
+        elif isinstance(data, list):
+            lst = []
+            for string in data:
+                lst.append(self.__Seperator__(string, **options))
+            return lst
+        else:
+            raise TypeError("Inputted data is of wrong type. Must be string of list of strings.")
+
+    # Add a "\n" after the maxspace limit for the inputted string.
+    # (Note that we use keywords here instead of **kwargs to catch errors if parameter is misspelled)
+    def __Seperator__(self, data, *, maxspace=10):
+        return textwrap.fill(data, width=maxspace, break_long_words=False)
