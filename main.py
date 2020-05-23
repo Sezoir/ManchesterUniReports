a40ee78493c4cc2988a4c609262f12cde2fb225f
Sezoir <joshmottley0@gmail.com>
2020-05-23 15:41:53 +0100

    - Moved testing of memory/time in main.py to separate function. - Cleaned up main.py.

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ import pandas as pd
 # General array math library
 import numpy as np
 
+def usage(func):
+    tracemalloc.start()
+    t = time.time()
+    fun = func()
+    elapsed = time.time() - t
+    print(f"Time elapsed is {elapsed} seconds")
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+    tracemalloc.stop()
+    return fun
+
 def main():
     # Set properties of console for pandas
     desired_width = 320
@@ -23,14 +34,7 @@ def main():
     pd.set_option('display.max_columns', 5)
 
     # Test reports
-    tracemalloc.start()
-    t = time.time()
-    cons = contr.Controllers()
-    elapsed = time.time() - t
-    print(elapsed)
-    current, peak = tracemalloc.get_traced_memory()
-    print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
-    tracemalloc.stop()
+    cons = usage(contr.Controllers)
 
     # Test Presenter class with filters
     # prest = pres.Presenter()
@@ -39,26 +43,6 @@ def main():
     # prest.addFilters({"Equals": [["internal", True], ["standardjobtype", True]],
     #                   "Date": [["created_at", "2016-1-1", "2018-5-6"]]})
     # print(prest.getTable(["school", "man_hours"]))
-
-    # Test speed of Repository class
-    # t = time.time()
-    # re = repo.Repository()
-    # elapsed = time.time() - t
-    # print(elapsed)
-    # print(re.mJobs)
-
-    # tracemalloc.start()
-    # t = time.time()
-    # re = repo.Repository()
-    # print(re.mJobs[re.mJobs.man_hours == 0])
-    # elapsed = time.time() - t
-    # print(elapsed)
-    # print(re.mJobs)
-    # current, peak = tracemalloc.get_traced_memory()
-    # print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
-    # tracemalloc.stop()
-    #
-    # del re
 
 
     return
